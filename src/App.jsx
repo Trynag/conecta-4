ca7c8912743f3f1d40786e1ec88656d9c0597da8
trynag <juangiraldocx@gmail.com>
2024-06-13 19:22:24 -0500

    Change [App] move row to col in for loop to sync visualize of board

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,24 @@ import { useState } from 'react'
 import { Square } from './components/Square'
 import { TURNS } from './logic/constants'
 
-const CLASS_NAME_COLUMNS = [
-  'first',
-  'second',
-  'third',
-  'fourth',
-  'fifth',
-  'sixth',
-  'seventh'
-]
+// Mantener la fila, mover columna
 
 function App () {
   const [turn, setTurn] = useState(TURNS.X)
   const [board, setBoard] = useState(Array.from({ length: 7 }, () => Array(6).fill(null)))
 
-  const updateBoard = ({ rowIndex, colIndex }) => {
+  const updateBoard = ({ rowIndex }) => {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
     const newBoard = [...board].map(row => [...row])
-    console.log(newBoard)
 
-    console.log(rowIndex, colIndex)
-
-    for (let rowIndex = 6; rowIndex >= 0; rowIndex--) {
+    for (let colIndex = 5; colIndex >= 0; colIndex--) {
       if (newBoard[rowIndex][colIndex] === null) {
         newBoard[rowIndex][colIndex] = turn
         break
       }
     }
-
     setBoard(newBoard)
   }
 
@@ -42,21 +30,23 @@ function App () {
         <section>
           <h1>Conecta 4</h1>
           <div className='game'>
-          {board.map((row, rowIndex) => (
-            <div key={rowIndex} className="row">
-              {row.map((cell, colIndex) => {
-                return (
-                  <Square
-                    key={`${rowIndex}-${colIndex}`}
-                    index={colIndex}
-                    updateBoard={() => updateBoard({ rowIndex, colIndex })}
-                  >
-                    {cell}
-                  </Square>
-                )
-              })}
-            </div>
-          ))}
+            {
+              board.map((row, rowIndex) => (
+                <div key={rowIndex} className="row">
+                  {row.map((cell, colIndex) => {
+                    return (
+                      <Square
+                        key={`${rowIndex}-${colIndex}`}
+                        index={colIndex}
+                        updateBoard={() => updateBoard({ rowIndex })}
+                      >
+                        {cell}
+                      </Square>
+                    )
+                  })}
+                </div>
+              ))
+            }
           </div>
         </section>
         <section className='turn'>
